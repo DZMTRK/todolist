e459c4a4dfe46b18dd9d697003ef61eb361537f4
DZMTRK <5286082@gmail.com>
2023-01-06 16:11:29 +0300

    feat: add css styling via bootstrap

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React from 'react';
+
+import './todo-list-item.css';
 
 const TodoListItem = ({ label, important = false }) => {
 
-    const style = {
-        color: important ? 'tomato': 'black'
-    };
+  const style = {
+    color: important ? 'steelblue' : 'black',
+    fontWeight: important ? 'bold' : 'normal'
+  };
 
-    return  <span style={ style }>{ label }</span>;
-}
+  return (
+    <span className="todo-list-item row">
+      <span
+        className="todo-list-item-label col-9"
+        style={style}>
+        {label}
+      </span>
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm col">
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button type="button"
+              className="btn btn-outline-danger btn-sm col">
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
+};
 
 export default TodoListItem;
